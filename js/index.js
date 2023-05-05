8fdf1c09d831194a8e14e6344b0704f2bcc30e03
Kevin <nevikmp@gmail.com>
2023-05-05 13:22:49 -0300

    tarea

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let name = prompt('Escriba su nombre')
+let last = prompt('Escriba su apellido')
+let birt = prompt('Escriba su año de nacimiento')
+const year = 2023
+
+let edad = year-birt
+
+alert(name+ ' '+last+' este año cumplis '+ edad+' años!!')
